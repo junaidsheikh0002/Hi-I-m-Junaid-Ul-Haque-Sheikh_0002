012158a1b7c8a7349b86e44059555ae07983098c
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-10-21 08:13:03 +0500

    https://github.com/junaidsheikh0002/Hi-I-m-Junaid-Ul-Haque-Sheikh_0002/upload
    https://github.com/junaidsheikh0002/Hi-I-m-Junaid-Ul-Haque-Sheikh_0002/upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', () => {
+        alert('View My Work clicked!');
+        // You can add more functionality here, like navigating to another section or page.
+    });
+});
